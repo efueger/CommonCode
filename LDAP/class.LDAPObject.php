e0b9b6848b485dd21f60d5475ddebe83e9c50eec
Problem <pboyd04@gmail.com>
2015-06-04 13:29:12 -0500

    Allow for serialization of generic LDAP objects

--- LDAP/class.LDAPObject.php
+++ LDAP/class.LDAPObject.php
@@ -10,6 +10,30 @@ class LDAPObject extends \SerializableObject
          parent::__construct($array);
          $this->server = $server;
      }
+
+     public function jsonSerialize()
+     {
+         $ret = array();
+         foreach ($this as $key => $value)
+         {
+            if($key === 'server' || $key === 'count') continue;
+            if(is_numeric($key)) continue;
+            if($key === 'jpegphoto')
+            {
+                $ret[$key] = base64_encode($value[0]);
+                continue;
+            }
+            if(is_array($value) && $value['count'] === 1)
+            {
+                $ret[$key] = $value[0];
+            }
+            else
+            {
+                $ret[$key] = $value;
+            }
+         }
+         return $ret;
+     }
 }
 
 ?>
