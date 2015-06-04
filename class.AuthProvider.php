4f5746f625204398d20e919495c6c5e8ec94ee2f
Problem <pboyd04@gmail.com>
2015-06-04 13:27:11 -0500

    Provide generic access to authentication frameworks

--- class.AuthProvider.php
+++ class.AuthProvider.php
@@ -47,6 +47,156 @@ class AuthProvider extends Singleton
         $auth = new $method_name();
         return $auth->get_user($data);
     }
+
+    public function get_group_by_name($method_name, $name)
+    {
+        if($method_name === false)
+        {
+            $ret = false;
+            $res = false;
+            $count = count($this->methods);
+            for($i = 0; $i < $count; $i++)
+            {
+                $res = $this->methods[$i]->get_group_by_name($name);
+                if($res !== false)
+                {
+                    if($ret === false)
+                    {
+                        $ret = $res;
+                    }
+                    else
+                    {
+                        $ret->merge($res);
+                    }
+                }
+            }
+            return $ret;
+        }
+        else
+        {
+            $auth = new $method_name();
+            return $auth->get_group_by_name($name);
+        }
+    }
+
+    public function get_users_by_filter($method_name, $filter, $select=false, $top=false, $skip=false, $orderby=false)
+    {
+        if($method_name === false)
+        {
+            $ret = false;
+            $res = false;
+            $count = count($this->methods);
+            for($i = 0; $i < $count; $i++)
+            {
+                $res = $this->methods[$i]->get_users_by_filter($filter, $select, $top, $skip, $orderby);
+                if($res !== false)
+                {
+                    if($ret === false)
+                    {
+                        $ret = $res;
+                    }
+                    else
+                    {
+                        $ret->merge($res);
+                    }
+                }
+            }
+            return $ret;
+        }
+        else
+        {
+            $auth = new $method_name();
+            return $auth->get_users_by_filter($filter, $select, $top, $skip, $orderby);
+        }
+    }
+
+    public function get_groups_by_filter($method_name, $filter, $select=false, $top=false, $skip=false, $orderby=false)
+    {
+        if($method_name === false)
+        {
+            $ret = false;
+            $res = false;
+            $count = count($this->methods);
+            for($i = 0; $i < $count; $i++)
+            {
+                $res = $this->methods[$i]->get_groups_by_filter($filter, $select, $top, $skip, $orderby);
+                if($res !== false)
+                {
+                    if($ret === false)
+                    {
+                        $ret = $res;
+                    }
+                    else
+                    {
+                        $ret->merge($res);
+                    }
+                }
+            }
+            return $ret;
+        }
+        else
+        {
+            $auth = new $method_name();
+            return $auth->get_groups_by_filter($filter, $select, $top, $skip, $orderby);
+        }
+    }
+
+    public function get_active_user_count($method_name = false)
+    {
+        if($method_name === false)
+        {
+            $user_count = 0;
+            $count = count($this->methods);
+            for($i = 0; $i < $count; $i++)
+            {
+                $user_count += $this->methods[$i]->get_active_user_count();
+            }
+            return $user_count;
+        }
+        else
+        {
+            $auth = new $method_name();
+            return $auth->get_active_user_count();
+        }
+    }
+
+    public function get_pending_user_count($method_name = false)
+    {
+        if($method_name === false)
+        {
+            $user_count = 0;
+            $count = count($this->methods);
+            for($i = 0; $i < $count; $i++)
+            {
+                $user_count += $this->methods[$i]->get_pending_user_count();
+            }
+            return $user_count;
+        }
+        else
+        {
+            $auth = new $method_name();
+            return $auth->get_pending_user_count();
+        }
+    }
+
+    public function get_group_count($method_name = false)
+    {
+        if($method_name === false)
+        {
+            $user_count = 0;
+            $count = count($this->methods);
+            for($i = 0; $i < $count; $i++)
+            {
+                $user_count += $this->methods[$i]->get_group_count();
+            }
+            return $user_count;
+        }
+        else
+        {
+            $auth = new $method_name();
+            return $auth->get_group_count();
+        }
+    }
 }
 /* vim: set tabstop=4 shiftwidth=4 expandtab: */
 ?>
