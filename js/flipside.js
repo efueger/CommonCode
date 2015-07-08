08686d08a3d9b7ea312c051c3a957db362bec115
Problem <pboyd04@gmail.com>
2015-07-08 15:58:19 +0000

    Add support for detecting image upload support from browser

--- js/flipside.js
+++ js/flipside.js
@@ -192,6 +192,17 @@ function browser_supported()
     }
 }
 
+function browser_supports_image_upload()
+{
+    if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/))
+    {
+        return false;
+    }
+    var elem = document.createElement('input');
+    elem.type = 'file';
+    return !elem.disabled;
+}
+
 function flipside_init()
 {
     browser_supported();
