140f255c5255d9bb976ce6d725484369ab386c2a
Problem <pboyd04@gmail.com>
2015-12-19 19:50:00 +0000

    Fix broken about links

--- class.FlipPage.php
+++ class.FlipPage.php
@@ -389,9 +389,9 @@ class FlipPage extends WebPage
             $this->addLink('Logout', $this->logout_url);
         }
         $about_menu = array(
-            'Burning Flipside'=>'http://www.burningflipside.com/about/event',
-            'AAR, LLC'=>'http://www.burningflipside.com/LLC',
-            'Privacy Policy'=>'http://www.burningflipside.com/about/privacy'
+            'Burning Flipside'=>'https://www.burningflipside.com/about/event',
+            'AAR, LLC'=>'https://www.burningflipside.com/organization/aar',
+            'Privacy Policy'=>'https://www.burningflipside.com/about/privacy'
         );
         $this->addLink('About', 'http://www.burningflipside.com/about', $about_menu);
     }
