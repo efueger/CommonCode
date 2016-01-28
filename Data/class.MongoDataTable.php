ded38f97e3fbedcb2a69c53ddb82c0c02ee6f052
Problem <pboyd04@gmail.com>
2016-01-28 23:29:39 +0000

    Fix issue with MongoDataTable->count

--- Data/class.MongoDataTable.php
+++ Data/class.MongoDataTable.php
@@ -23,7 +23,7 @@ class MongoDataTable extends DataTable
         $criteria = array();
         if($filter !== false)
         {
-            if($filter instanceof Data\Filter)
+            if($filter instanceof \Data\Filter)
             {
                 $criteria = $filter->to_mongo_filter();
             }
