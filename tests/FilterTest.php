f7308e759c51c650b83e8fb524b2f552bc1659c5
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-05-06 18:46:02 +0000

    Add more filter tests

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -2,7 +2,7 @@
 require_once('Autoload.php');
 class FilterTest extends PHPUnit_Framework_TestCase
 {
-    public function testParsing()
+    public function testOpParsing()
     {
         $filter = new \Data\Filter('a eq b');
         $clauses = $filter->getChildren();
@@ -67,6 +67,51 @@ class FilterTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($clause->op, '<=');
         $this->assertEquals($clause->var1, 'a');
         $this->assertEquals($clause->var2, 'b');
+    }
+
+    public function testParenseParsing()
+    {
+        $filter = new \Data\Filter('(a eq b)');
+        $clauses = $filter->getChildren();
+        $this->assertCount(1, $clauses);
+        $clause = $clauses[0];
+        $this->assertEquals($clause->op, '=');
+        $this->assertEquals($clause->var1, 'a');
+        $this->assertEquals($clause->var2, 'b');
+    }
+
+    public function testAndParsing()
+    {
+        $filter = new \Data\Filter('a eq b and c eq d');
+        $clauses = $filter->getChildren();
+        $this->assertCount(3, $clauses);
+        $clause = $clauses[0];
+        $this->assertEquals($clause->op, '=');
+        $this->assertEquals($clause->var1, 'a');
+        $this->assertEquals($clause->var2, 'b');
+        $clause = $clauses[1];
+        $this->assertEquals($clause, 'and');
+        $clause = $clauses[2];
+        $this->assertEquals($clause->op, '=');
+        $this->assertEquals($clause->var1, 'c');
+        $this->assertEquals($clause->var2, 'd');
+    }
+
+    public function testOrParsing()
+    {
+        $filter = new \Data\Filter('a eq b or c eq d');
+        $clauses = $filter->getChildren();
+        $this->assertCount(3, $clauses);
+        $clause = $clauses[0];
+        $this->assertEquals($clause->op, '=');
+        $this->assertEquals($clause->var1, 'a');
+        $this->assertEquals($clause->var2, 'b');
+        $clause = $clauses[1];
+        $this->assertEquals($clause, 'or');
+        $clause = $clauses[2];
+        $this->assertEquals($clause->op, '=');
+        $this->assertEquals($clause->var1, 'c');
+        $this->assertEquals($clause->var2, 'd');
     }
 }
 /* vim: set tabstop=4 shiftwidth=4 expandtab: */
