77c87715f0d9bf69baa2912e01e950840d70f31b
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-05-09 19:06:30 +0000

    Add Page Valid Unit Test

---
+++ tests/PageValidTest.php
@@ -0,0 +1,22 @@
+<?php
+require_once('Autoload.php');
+require_once('./tests/helpers/HTML5Validate.php');
+class PageValidTest extends PHPUnit_Framework_TestCase
+{
+    public function testEmptyPage()
+    {
+        $GLOBALS['FLIPSIDE_SETTINGS_LOC'] = './tests/helpers';
+        $GLOBALS['BROWSCAP_CACHE']        = './tests/helpers';
+        $page = new FlipPage('Test', false);
+        ob_start();
+        $page->print_page();
+        $html = ob_get_contents();
+        ob_end_clean();
+
+
+        $validator = new HTML5Validate();
+        $result = $validator->Assert($html);
+        $this->assertTrue($result, $validator->message);
+    }
+}
+?>
