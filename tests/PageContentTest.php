730fa5cc59455d303b55ba1c13c450e16322d363
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-05-10 19:55:04 +0000

    Add IE test cases

--- tests/PageContentTest.php
+++ tests/PageContentTest.php
@@ -71,5 +71,62 @@ class PageContentTest extends PHPUnit_Framework_TestCase
             }
         }
     }
+
+    public function testIERendering()
+    {
+        $GLOBALS['FLIPSIDE_SETTINGS_LOC'] = './tests/helpers';
+        $GLOBALS['BROWSCAP_CACHE']        = './tests/helpers';
+        $page = new FlipPage('Test');
+        $page->browser = new stdClass();
+        $page->browser->Browser = 'IE';
+        $page->browser->MajorVer = 9;
+        ob_start();
+        $page->print_page();
+        $html = ob_get_contents();
+        ob_end_clean();
+
+        $doc = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $doc->loadHTML($html);
+        $elements = $doc->getElementsByTagName('meta');
+        $this->assertEquals(2, $elements->length);
+        $node = $elements->item(0);
+        $this->assertTrue($node->hasAttribute('http-equiv'));
+        $value = $node->getAttribute('http-equiv');
+        $this->assertEquals('X-UA-Compatible', $value);
+        $this->assertTrue($node->hasAttribute('content'));
+        $value = $node->getAttribute('content');
+        $this->assertEquals('IE=edge', $value);
+
+        $page = new FlipPage('Test');
+        $page->browser = new stdClass();
+        $page->browser->Browser = 'IE';
+        $page->browser->MajorVer = 8;
+        ob_start();
+        $page->print_page();
+        $html = ob_get_contents();
+        ob_end_clean();
+
+        $doc = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $doc->loadHTML($html);
+        $elements = $doc->getElementsByTagName('script');
+        $this->assertEquals(5, $elements->length);
+        $found = false;
+        for($i = 0; $i < $elements->length; $i++)
+        {
+            $node = $elements->item($i);
+            if($node->hasAttribute('src'))
+            {
+                $attrib = $node->getAttribute('src');
+                if(strstr($attrib, 'html5.js') !== false)
+                {
+                    $found = true;
+                    break;
+                }
+            }
+        }
+        $this->assertTrue($found);
+    }
 }
 ?>
